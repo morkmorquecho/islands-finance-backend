7f5ba8def742f1824870608fd2e7cb526fd6486d
morkmorquecho <mork.morquecho@gmail.com>
2026-09-11 16:40:18 -0600

    ad backend filters

--- config/settings.py
+++ config/settings.py
@@ -246,6 +246,7 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
