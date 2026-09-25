1a375721b233ecbf118459a4d8d4c401ec4dfbf9
morkmorquecho <mork.morquecho@gmail.com>
2026-09-24 20:10:43 -0600

    new entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,9 @@ python manage.py collectstatic --noinput
 echo "▶ Iniciando servidor..."
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:$PORT \
-    --workers 3 \
+    --workers 2 \
+    --worker-class gthread \
+    --threads 4 \
     --timeout 120 \
     --access-logfile - \
     --error-logfile -
